474ac17376f6bb4914098376c9dc71ad4affa1b8
Andreea Mocrii <mocriiandreea@yahoo.com>
2022-03-24 21:32:32 +0200

    first commit

---
+++ Proiect_TPBD/Proiect_TPBD/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Proiect_TPBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new Proiect_TPBD.DataSet1();
+            this.tESTBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tESTTableAdapter = new Proiect_TPBD.DataSet1TableAdapters.TESTTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nUMEDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sALARDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nUMEDataGridViewTextBoxColumn,
+            this.sALARDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tESTBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(67, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(505, 165);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tESTBindingSource
+            // 
+            this.tESTBindingSource.DataMember = "TEST";
+            this.tESTBindingSource.DataSource = this.dataSet1;
+            // 
+            // tESTTableAdapter
+            // 
+            this.tESTTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            this.iDDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nUMEDataGridViewTextBoxColumn
+            // 
+            this.nUMEDataGridViewTextBoxColumn.DataPropertyName = "NUME";
+            this.nUMEDataGridViewTextBoxColumn.HeaderText = "NUME";
+            this.nUMEDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nUMEDataGridViewTextBoxColumn.Name = "nUMEDataGridViewTextBoxColumn";
+            this.nUMEDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // sALARDataGridViewTextBoxColumn
+            // 
+            this.sALARDataGridViewTextBoxColumn.DataPropertyName = "SALAR";
+            this.sALARDataGridViewTextBoxColumn.HeaderText = "SALAR";
+            this.sALARDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.sALARDataGridViewTextBoxColumn.Name = "sALARDataGridViewTextBoxColumn";
+            this.sALARDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tESTBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource tESTBindingSource;
+        private DataSet1TableAdapters.TESTTableAdapter tESTTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nUMEDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sALARDataGridViewTextBoxColumn;
+    }
+}
+
